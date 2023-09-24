8cf80e46bccf558cb7a47c6506ff9b1e1a308a88
Lawrence Amoafo <rekenburg777@gmail.com>
2023-09-24 14:29:28 +0000

    Add project details

---
+++ src/components/data/projectData.jsx
@@ -0,0 +1,56 @@
+import image1 from "../../assets/splash.jpg";
+import image4 from "../../assets/fourth-project.jpg";
+import image6 from "../../assets/sixth-project.jpg";
+
+const projectData = [
+  {
+    id: 1,
+    title: "Grow Rich",
+    description: "This is a Budgeting App",
+    image_url: `${image1}`,
+    url: "https://grow-rich.onrender.com/",
+    source: "https://github.com/Kwaku28/Grow-Rich",
+  },
+  {
+    id: 2,
+    title: "Space traveler's hub",
+    description: "Book rockets and join missions",
+    image_url: `${image4}`,
+    url: "https://space-travelers-hub-sum7.onrender.com/",
+    source: "https://github.com/Kwaku28/Space-Travelers-Hub",
+  },
+  {
+    id: 3,
+    title: "Bookstore",
+    description: "Keep track of your reading progress",
+    image_url: "https://via.placeholder.com/150",
+    url: "https://bookstore-7igy.onrender.com/",
+    source: "https://github.com/Kwaku28/Bookstore",
+  },
+  {
+    id: 4,
+    title: "Leaderboard",
+    description: "Keep track of your scores",
+    image_url: "https://via.placeholder.com/150",
+    url: "https://kwaku28.github.io/leaderboard/dist/",
+    source: "https://github.com/Kwaku28/leaderboard",
+  },
+  {
+    id: 5,
+    title: "Architectural Summit",
+    description: "A sample website of a summit",
+    image_url: "https://via.placeholder.com/150",
+    url: "https://kwaku28.github.io/architectural-summit/",
+    source: "https://github.com/Kwaku28/architectural-summit",
+  },
+  {
+    id: 6,
+    title: "Resort Booking",
+    description: "Book a resort for your next vacation",
+    image_url: `${image6}`,
+    url: "https://resort-app-frontend.onrender.com",
+    source: "https://github.com/Leeoasis/resort-booking-frontend-app",
+  },
+];
+
+export default projectData;
